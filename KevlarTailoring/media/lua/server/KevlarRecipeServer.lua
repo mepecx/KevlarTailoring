cd7050640ee84db91565e5d8923ff6c9118ebead
mepecx <102268428+mepecx@users.noreply.github.com>
2022-06-28 03:41:01 -0400

    Add files via upload

---
+++ KevlarTailoring/media/lua/server/KevlarRecipeServer.lua
@@ -0,0 +1,5 @@
+require('recipecode')
+
+function Recipe.GetItemTypes.RipClothing_Kevlar(scriptItems)
+    RipClothing_GetItemTypes_XXX(scriptItems, "Kevlar")
+end
---
+++ KevlarTailoring/media/lua/shared/KevlarDef.lua
@@ -0,0 +1,6 @@
+ClothingRecipesDefinitions["FabricType"]["Kevlar"] = {}
+ClothingRecipesDefinitions["FabricType"]["Kevlar"].material = "KevlarTailoring.KevlarStrips";
+ClothingRecipesDefinitions["FabricType"]["Kevlar"].tools = "Base.Scissors";
+ClothingRecipesDefinitions["FabricType"]["Kevlar"].noSheetRope = true;
+
+
